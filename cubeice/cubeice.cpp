168aee563c29304a82a45c011ed37caa8b71fc7a
clown <tt.clown@gmail.com>
2010-12-16 21:42:00 +0900

    modify archiver.

--- cubeice/cubeice.cpp
+++ cubeice/cubeice.cpp
@@ -1,13 +1,14 @@
 #include "cubeice.h"
-
 #include <windows.h>
 
-int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR pCmdLine, int showCmd) {
-	cube::cmdline::separator sep(_T(" \t"), _T("\""), _T(""));
-	cube::cmdline::splitter args(pCmdLine, sep);
-	cube::cmdline::splitter::iterator pos = args.begin();
+cubeice::user_setting UserSetting;
 
-	cube::archiver ar;
+int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR pCmdLine, int showCmd) {
+	cubeice::cmdline::separator sep(_T(" \t"), _T("\""), _T(""));
+	cubeice::cmdline::splitter args(pCmdLine, sep);
+	cubeice::cmdline::splitter::iterator pos = args.begin();
+
+	cubeice::archiver ar(UserSetting);
 	if (pos != args.end() && *pos == _T("-c")) ar.compress(pos, args.end());
 	else if (pos != args.end() && *pos == _T("-x")) ar.decompress(pos, args.end());
 	else return -1;
