d901dc5417659a247a3c9f5fd8c7af90bb3eaaa7
clown <tt.clown@gmail.com>
2010-12-24 22:52:18 +0900

    temp commit.

--- cubeice/cubeice.cpp
+++ cubeice/cubeice.cpp
@@ -44,7 +44,7 @@ int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPTSTR pCmdLine, int 
 
 	cubeice::archiver ar(UserSetting);
 	if (pos != args.end() && pos->compare(0, 3, _T("/c:")) == 0) ar.compress(pos, args.end());
-	else if (pos != args.end() && *pos == _T("/x")) ar.decompress(pos, args.end());
+	else if (pos != args.end() && pos->compare(0, 3, _T("/x:")) == 0) ar.decompress(pos, args.end());
 	else return -1;
 
 	return 0;
