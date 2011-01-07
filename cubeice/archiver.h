d5968d3a6995ea264ef3a12efdb45d41372b41ea
clown <tt.clown@gmail.com>
2011-01-07 14:02:44 +0900

    modify license descriptions.

--- cubeice/archiver.h
+++ cubeice/archiver.h
@@ -2,7 +2,7 @@
 /*
  *  archiver.h
  *
- *  Copyright (c) 2010 CubeSoft Inc. All rights reserved.
+ *  Copyright (c) 2010 CubeSoft Inc.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
