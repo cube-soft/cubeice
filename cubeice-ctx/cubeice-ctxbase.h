39827340d2d13196bd843504071bb41a130a8b4e
clown <tt.clown@gmail.com>
2011-01-12 15:15:11 +0900

    trivial modifications.

--- cubeice-ctx/cubeice-ctxbase.h
+++ cubeice-ctx/cubeice-ctxbase.h
@@ -1,3 +1,37 @@
+/* ------------------------------------------------------------------------- */
+/*
+ *  cubeice-ctxbase.h
+ *
+ *  Copyright (c) 2010 CubeSoft Inc.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    - No names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior written
+ *      permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Last-modified: Wed 12 Jan 2011 14:28:04 JST
+ */
+/* ------------------------------------------------------------------------- */
 #ifndef	CUBEICE_CTXBASE_H
 #define	CUBEICE_CTXBASE_H
 
@@ -5,34 +39,55 @@ extern HINSTANCE	hDllInstance;
 
 namespace cube {
 	namespace shlctxmenu {
-
+		/* ----------------------------------------------------------------- */
+		//  CShlCtxMenuBase
+		/* ----------------------------------------------------------------- */
 		class CShlCtxMenuBase : public IContextMenu3, IShellExtInit, IMenuInfo {
 		public:
+			/* ------------------------------------------------------------- */
+			//  constructor
+			/* ------------------------------------------------------------- */
 			CShlCtxMenuBase() : refCount( 0L ), hInstance( hDllInstance ) {}
+			
+			/* ------------------------------------------------------------- */
+			//  destructor
+			/* ------------------------------------------------------------- */
 			virtual ~CShlCtxMenuBase() {}
-
-			// IUnknown members
+			
+			/* ------------------------------------------------------------- */
+			/*
+			 *  QueryInterface
+			 *
+			 *  IUnknown members
+			 */
+			/* ------------------------------------------------------------- */
 			STDMETHODIMP QueryInterface( REFIID riid, LPVOID FAR *ppv ) {
 				*ppv = NULL;
-
+				
 				if( IsEqualIID( riid, IID_IUnknown ) || IsEqualIID( riid, IID_IContextMenu ) )
 					*ppv = static_cast<LPCONTEXTMENU>( this );
 				else if( IsEqualIID( riid, IID_IShellExtInit ) )
 					*ppv = static_cast<LPSHELLEXTINIT>( this );
 				else
 					return E_NOINTERFACE;
-
+				
 				AddRef();
-
+				
 				return NOERROR;
 			}
-
+			
+			/* ------------------------------------------------------------- */
+			//  AddRef
+			/* ------------------------------------------------------------- */
 			STDMETHODIMP_(ULONG) AddRef() {
-				return InterlockedIncrement( &refCount );
+				return InterlockedIncrement( reinterpret_cast<LONG*>(&refCount) );
 			}
-
+			
+			/* ------------------------------------------------------------- */
+			//  Release
+			/* ------------------------------------------------------------- */
 			STDMETHODIMP_(ULONG) Release() {
-				ULONG res = InterlockedDecrement( &refCount );
+				ULONG res = InterlockedDecrement( reinterpret_cast<LONG*>(&refCount) );
 
 				if( res )
 					return res;
@@ -41,29 +96,35 @@ namespace cube {
 
 				return 0L;
 			}
-
-			// IContextMenu members
+			
+			/* ------------------------------------------------------------- */
+			/*
+			 *  QueryContextMenu
+			 *
+			 *  IContextMenu members
+			 */
+			/* ------------------------------------------------------------- */
 			STDMETHODIMP QueryContextMenu( HMENU hMenu, UINT indexMenu, UINT idCmdFirst, UINT idCmdLast, UINT uFlags ) {
 				UINT						idCmd = idCmdFirst;
-
+				
 				if( uFlags & CMF_DEFAULTONLY )
 					return NO_ERROR;
-
+				
 				if( !ctxSetting )
 					return NO_ERROR;
-
+				
 				// Add separator
 				InsertMenu( hMenu, indexMenu++, MF_BYPOSITION | MF_SEPARATOR, 0, NULL );
-
+				
 				for( unsigned int i = 0 ; MenuItem[i].stringA ; ++i ) {
 					if( MenuItem[i].dispSetting && !( ctxSetting & MenuItem[i].dispSetting ) )
 						continue;
 					
 					if( MenuItem[i].dispSetting == DECOMPRESS_FLAG && !this->is_decompress(fileList.begin(), fileList.end()))
 						continue;
-
+					
 					MENUITEMINFO		miinfo;
-
+					
 					ZeroMemory( &miinfo, sizeof( miinfo ) );
 					miinfo.cbSize			= sizeof( miinfo );
 					miinfo.fMask			= MIIM_FTYPE | MIIM_STRING | MIIM_ID;
@@ -74,7 +135,7 @@ namespace cube {
 #else	// UNICODE
 					miinfo.dwTypeData		= const_cast<LPWSTR>( MenuItem[i].stringW );
 #endif	// UNICODE
-
+					
 					if( MenuItem[i].submenu ) {
 						miinfo.fMask		|= MIIM_SUBMENU;
 						miinfo.hSubMenu		= CreateSubMenu( MenuItem[i].submenu, idCmd );
@@ -82,13 +143,13 @@ namespace cube {
 					
 					if( MenuItem[i].iconID != ICON_NOT_USED )
 						SetMenuIcon( MenuItem[i].iconID, miinfo );
-
+					
 					InsertMenuItem( hMenu, indexMenu++, TRUE, &miinfo );
 				}
-
+				
 				// Add separator
 				InsertMenu( hMenu, indexMenu++, MF_BYPOSITION | MF_SEPARATOR, 0, NULL );
-
+				
 				MENUINFO	mi;
 				ZeroMemory( &mi, sizeof( mi ) );
 				mi.cbSize	= sizeof( mi );
@@ -97,114 +158,146 @@ namespace cube {
 				mi.dwStyle	= ( mi.dwStyle & ~MNS_NOCHECK ) | MNS_CHECKORBMP;
 				mi.fMask	= MIM_STYLE | MIM_APPLYTOSUBMENUS;
 				SetMenuInfo( hMenu, &mi );
-
+				
 				return MAKE_SCODE( SEVERITY_SUCCESS, FACILITY_NULL, idCmd - idCmdFirst );
 			}
-
+			
+			/* ------------------------------------------------------------- */
+			//  InvokeCommand
+			/* ------------------------------------------------------------- */
 			STDMETHODIMP InvokeCommand( LPCMINVOKECOMMANDINFO lpcmi ) {
 				if( HIWORD( lpcmi->lpVerb ) )
 					return E_INVALIDARG;
-
+				
 				DWORD	index = 0;
-
+				
 				RecursiveInvokeCommand( MenuItem, lpcmi, index );
-
+				
 				return S_OK;
 			}
-
+			
 			STDMETHODIMP GetCommandString( UINT_PTR idCmd, UINT uFlags, UINT FAR *reserved, LPSTR pszName, UINT cchMax ) {
 				unsigned int	index = 0;
-
+				
 				RecursiveGetCommandString( MenuItem, index, idCmd, uFlags, pszName, cchMax );
-
+				
 				if( index < idCmd ) {
 					if( uFlags == GCS_VALIDATEA || uFlags == GCS_VALIDATEW )
 						return S_FALSE;
 				}
 				return S_OK;
 			}
-
-			// IContextMenu2 members
+			
+			/* ------------------------------------------------------------- */
+			/*
+			 *  HandleMenuMsg
+			 *
+			 *  IContextMenu2 members
+			 */
+			/* ------------------------------------------------------------- */
 			STDMETHODIMP HandleMenuMsg( UINT uMsg, WPARAM wParam, LPARAM lParam ) {
 				return S_OK;
 			}
-
-			// IContextMenu3 members
+			
+			/* ------------------------------------------------------------- */
+			/* 
+			 *  HandleMenuMsg2
+			 *
+			 *  IContextMenu3 members
+			 */
+			/* ------------------------------------------------------------- */
 			STDMETHODIMP HandleMenuMsg2( UINT uMsg, WPARAM wParam, LPARAM lParam, LRESULT *plResult ) {
 				return S_OK;
 			}
-
-			// IShellExtInit methods
+			
+			/* ------------------------------------------------------------- */
+			/*
+			 *  Initialize
+			 *
+			 *  IShellExtInit methods
+			 */
+			/* ------------------------------------------------------------- */
 			STDMETHODIMP Initialize( LPCITEMIDLIST pIDFolder, LPDATAOBJECT pDataObj, HKEY hKeyID ) {
 				HRESULT			hr;
 				FORMATETC		fme;
 				STGMEDIUM		stgm;
 				HDROP			hDrop;
 				UINT			numoffiles;
-
+				
 				cubeice::user_setting	setting;
 				ctxSetting = setting.context_flags();
-
+				
 				fme.cfFormat	= CF_HDROP;
 				fme.ptd			= NULL;
 				fme.dwAspect	= DVASPECT_CONTENT;
 				fme.lindex		= -1;
 				fme.tymed		= TYMED_HGLOBAL;
-
+				
 				hr = pDataObj->GetData( &fme, &stgm );
 				if( FAILED( hr ) )
 					return hr;
-
+				
 				hDrop = static_cast<HDROP>( GlobalLock( stgm.hGlobal ) );
 				numoffiles = DragQueryFile( hDrop, static_cast<UINT>( -1 ), NULL, 0 );
-
+				
 				for( UINT i = 0 ; i < numoffiles ; ++i ) {
 					UINT	bufsize;
 					TCHAR	*buffer;
-
+					
 					bufsize = DragQueryFile( hDrop, i, NULL, 0 );
 					buffer = new TCHAR[bufsize+5];
 					DragQueryFile( hDrop, i, buffer, bufsize + 3 );
-
+					
 					fileList.push_back( tstring( buffer ) );
-
+					
 					delete[] buffer;
 				}
-
+				
 				GlobalUnlock( hDrop );
 				ReleaseStgMedium( &stgm );
-
+				
 				return S_OK;
 			}
-
-			// IMenuInfo methods
+			
+			/* ------------------------------------------------------------- */
+			//  GetFileList
+			/* ------------------------------------------------------------- */
 			const std::vector<tstring> &GetFileList() {
 				return fileList;
 			}
-
+			
+			/* ------------------------------------------------------------- */
+			//  GetFilePath
+			/* ------------------------------------------------------------- */
 			const tstring &GetFilePath( const std::size_t &index ) {
 				return fileList.at( index );
 			}
-
+			
+			/* ------------------------------------------------------------- */
+			//  GetSize
+			/* ------------------------------------------------------------- */
 			std::size_t GetSize() {
 				return fileList.size();
 			}
-
+			
 		protected:
 			HINSTANCE	hInstance;
 			virtual void SetMenuIcon( WORD iconID, MENUITEMINFO &miinfo ) = 0;
-
+			
 		private:
-			// Create sub menu
+			
+			/* ------------------------------------------------------------- */
+			//  CreateSubMenu
+			/* ------------------------------------------------------------- */
 			HMENU CreateSubMenu( const SUB_MENU_ITEM *smi, UINT &idCmd ) {
 				HMENU		hMenu = CreateMenu();
-
+				
 				for( unsigned int i = 0 ; smi[i].stringA ; ++i ) {
 					MENUITEMINFO		miinfo;
-
+					
 					if( smi[i].dispSetting && !( ctxSetting & smi[i].dispSetting ) )
 						continue;
-
+					
 					ZeroMemory( &miinfo, sizeof( miinfo ) );
 					miinfo.cbSize			= sizeof( miinfo );
 					miinfo.fMask			= MIIM_FTYPE | MIIM_STRING | MIIM_ID;
@@ -215,29 +308,31 @@ namespace cube {
 #else	// UNICODE
 					miinfo.dwTypeData		= const_cast<LPWSTR>( smi[i].stringW );
 #endif	// UNICODE
-
+					
 					if( smi[i].submenu ) {
 						miinfo.fMask		|= MIIM_SUBMENU;
 						miinfo.hSubMenu		= CreateSubMenu( smi[i].submenu, idCmd );
 					}
 					if( smi[i].iconID != ICON_NOT_USED )
 						SetMenuIcon( smi[i].iconID, miinfo );
-
+					
 					InsertMenuItem( hMenu, i, TRUE, &miinfo );
 				}
-
+				
 				return hMenu;
 			}
-
-			// Recursive invoke command
+			
+			/* ------------------------------------------------------------- */
+			//  RecursiveInvokeCommand
+			/* ------------------------------------------------------------- */
 			void RecursiveInvokeCommand( const SUB_MENU_ITEM *smi, LPCMINVOKECOMMANDINFO lpcmi, DWORD &index ) {
 				for( unsigned int i = 0 ; smi[i].stringA && index <= LOWORD( lpcmi->lpVerb ) ; ++i ) {
 					if( smi[i].dispSetting && !( ctxSetting & smi[i].dispSetting ) )
 						continue;
-
+					
 					if( smi[i].dispSetting == DECOMPRESS_FLAG && !this->is_decompress(fileList.begin(), fileList.end()))
 						continue;
-
+					
 					if( index == LOWORD( lpcmi->lpVerb ) ) {
 						MenuSelectedCallback( smi[i].arg, this );
 						++index;
@@ -247,11 +342,13 @@ namespace cube {
 					if( smi[i].submenu )
 						RecursiveInvokeCommand( smi[i].submenu, lpcmi, index );
 				}
-
+				
 				return;
 			}
-
-			// Recursive get command string
+			
+			/* ------------------------------------------------------------- */
+			//  RecursiveGetCommandString
+			/* ------------------------------------------------------------- */
 			void RecursiveGetCommandString( const SUB_MENU_ITEM *smi, unsigned int &index, UINT_PTR idCmd, UINT uFlags, LPSTR pszName, UINT cchMax ) {
 				for( unsigned int i = 0 ; smi[i].stringA && index <= idCmd ; ++i ) {
 					if( smi[i].dispSetting && !( ctxSetting & smi[i].dispSetting ) )
@@ -284,10 +381,13 @@ namespace cube {
 					if( smi[i].submenu )
 						RecursiveGetCommandString( smi[i].submenu, index, idCmd, uFlags, pszName, cchMax );
 				}
-
+				
 				return;
 			}
 			
+			/* ------------------------------------------------------------- */
+			//  is_decompress
+			/* ------------------------------------------------------------- */
 			template <class InputIterator>
 			bool is_decompress(InputIterator first, InputIterator last) {
 				static bool init_ = false;
--- cubeice-ctx/cubeice-ctxvista.h
+++ cubeice-ctx/cubeice-ctxvista.h
@@ -1,3 +1,37 @@
+/* ------------------------------------------------------------------------- */
+/*
+ *  cubeice-ctxvista.h
+ *
+ *  Copyright (c) 2010 CubeSoft Inc.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    - Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    - Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    - No names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior written
+ *      permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  Last-modified: Wed 12 Jan 2011 14:28:04 JST
+ */
+/* ------------------------------------------------------------------------- */
 #ifndef	CUBEICE_CTXVISTA_H
 #define	CUBEICE_CTXVISTA_H
 
@@ -11,30 +45,36 @@
 namespace cube {
 	namespace shlctxmenu {
 		namespace icon {
+			/* ------------------------------------------------------------- */
+			//  Icon
+			/* ------------------------------------------------------------- */
 			class Icon {
 			public:
+				/* --------------------------------------------------------- */
+				//  constructor
+				/* --------------------------------------------------------- */
 				Icon( HINSTANCE hInstance, WORD iconID ) : hBitmap( NULL ) {
-
+					
 					hUxTheme = LoadLibrary( TEXT( "UXTHEME.DLL" ) );
 					if( !hUxTheme )
 						return;
 					pfnGetBufferedPaintBits	= reinterpret_cast<FN_GetBufferedPaintBits>( GetProcAddress( hUxTheme, "GetBufferedPaintBits" ) );
 					pfnBeginBufferedPaint	= reinterpret_cast<FN_BeginBufferedPaint>( GetProcAddress( hUxTheme, "BeginBufferedPaint" ) );
 					pfnEndBufferedPaint		= reinterpret_cast<FN_EndBufferedPaint>( GetProcAddress( hUxTheme, "EndBufferedPaint" ) );
-
+					
 					if( !pfnGetBufferedPaintBits || !pfnBeginBufferedPaint || !pfnEndBufferedPaint )
 						return;
-
+					
 					HICON	hIcon = static_cast<HICON>( LoadImage( hInstance, MAKEINTRESOURCE( iconID ), IMAGE_ICON, ICON_X_SIZE, ICON_Y_SIZE, LR_DEFAULTCOLOR ) );
 					if( !hIcon )
 						return;
-
+					
 					HDC		hdc = CreateCompatibleDC( NULL );
 					if( !hdc ) {
 						DestroyIcon( hIcon );
 						return;
 					}
-
+					
 					BITMAPINFO	bmi;
 					ZeroMemory( &bmi, sizeof( bmi ) );
 					bmi.bmiHeader.biSize		= sizeof( bmi );
@@ -43,22 +83,22 @@ namespace cube {
 					bmi.bmiHeader.biWidth		= ICON_X_SIZE;
 					bmi.bmiHeader.biHeight		= ICON_Y_SIZE;
 					bmi.bmiHeader.biBitCount	= 32;
-
+					
 					hBitmap = CreateDIBSection( hdc, &bmi, DIB_RGB_COLORS, NULL, NULL, 0 );
 					if( !hBitmap ) {
 						DeleteDC( hdc );
 						DestroyIcon( hIcon );
 						return;
 					}
-
+					
 					HBITMAP	hOldBmp = static_cast<HBITMAP>( SelectObject( hdc, static_cast<HGDIOBJ>(hBitmap) ) );
-
+					
 					BLENDFUNCTION	bfAlpha = { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA };
 					BP_PAINTPARAMS	paintParams = {0};
 					paintParams.cbSize			= sizeof(paintParams);
 					paintParams.dwFlags			= BPPF_ERASE;
 					paintParams.pBlendFunction	= &bfAlpha;
-
+					
 					HDC				hdcBuffer;
 					RECT			rc;
 					SetRect( &rc, 0, 0, ICON_X_SIZE, ICON_Y_SIZE );
@@ -71,38 +111,48 @@ namespace cube {
 						pfnEndBufferedPaint( hPaintBuffer, TRUE );
 					}
 					SelectObject( hdc, static_cast<HGDIOBJ>(hOldBmp) );
-
+					
 					DeleteDC( hdc );
 					DestroyIcon( hIcon );
 				}
+				
+				/* --------------------------------------------------------- */
+				//  destructor
+				/* --------------------------------------------------------- */
 				~Icon() {
 					if( hBitmap )
 						DeleteObject( hBitmap );
 					FreeLibrary( hUxTheme );
 				}
-
+				
+				/* --------------------------------------------------------- */
+				//  GetBitmap
+				/* --------------------------------------------------------- */
 				HBITMAP GetBitmap() {
 					return hBitmap;
 				}
-
+				
 			private:
 				typedef HRESULT ( WINAPI *FN_GetBufferedPaintBits )( HPAINTBUFFER hBufferedPaint, RGBQUAD **ppbBuffer, int *pcxRow );
 				typedef HPAINTBUFFER ( WINAPI *FN_BeginBufferedPaint )( HDC hdcTarget, const RECT *prcTarget, BP_BUFFERFORMAT dwFormat, BP_PAINTPARAMS *pPaintParams, HDC *phdc );
 				typedef HRESULT ( WINAPI *FN_EndBufferedPaint )( HPAINTBUFFER hBufferedPaint, BOOL fUpdateTarget );
 				typedef DWORD ARGB;
-
+				
 				HMODULE					hUxTheme;
 				FN_GetBufferedPaintBits	pfnGetBufferedPaintBits;
 				FN_BeginBufferedPaint	pfnBeginBufferedPaint;
 				FN_EndBufferedPaint		pfnEndBufferedPaint;
 				HBITMAP					hBitmap;
-
+				
+				/* --------------------------------------------------------- */
+				//  ConvertBufferToPARGB32
+				/* --------------------------------------------------------- */
 				HRESULT ConvertBufferToPARGB32( HPAINTBUFFER hPaintBuffer, HDC hdc, HICON hicon, SIZE &sizIcon )
 				{
 					RGBQUAD		*prgbQuad;
 					int			cxRow;
 					HRESULT		hr = pfnGetBufferedPaintBits( hPaintBuffer, &prgbQuad, &cxRow );
-
+					
 					if( SUCCEEDED( hr ) ) {
 						ARGB	*pargb = reinterpret_cast<ARGB *>(prgbQuad);
 						if( !HasAlpha( pargb, sizIcon, cxRow ) ) {
@@ -111,16 +161,19 @@ namespace cube {
 								if( info.hbmMask ) {
 									hr = ConvertToPARGB32( hdc, pargb, info.hbmMask, sizIcon, cxRow );
 								}
-
+								
 								DeleteObject( info.hbmColor );
 								DeleteObject( info.hbmMask );
 							}
 						}
 					}
-
+					
 					return hr;
 				}
-
+				
+				/* --------------------------------------------------------- */
+				//  HasAlpha
+				/* --------------------------------------------------------- */
 				bool HasAlpha( ARGB *pargb, SIZE& sizImage, int cxRow )
 				{
 					ULONG	cxDelta = cxRow - sizImage.cx;
@@ -131,10 +184,13 @@ namespace cube {
 						}
 						pargb += cxDelta;
 					}
-
+					
 					return false;
 				}
-
+				
+				/* --------------------------------------------------------- */
+				//  ConvertToPARGB32
+				/* --------------------------------------------------------- */
 				HRESULT ConvertToPARGB32( HDC hdc, ARGB *pargb, HBITMAP hbmp, SIZE &sizImage, int cxRow )
 				{
 					BITMAPINFO	bmi;
@@ -145,7 +201,7 @@ namespace cube {
 					bmi.bmiHeader.biWidth		= ICON_X_SIZE;
 					bmi.bmiHeader.biHeight		= ICON_Y_SIZE;
 					bmi.bmiHeader.biBitCount	= 32;
-
+					
 					HRESULT		hr = E_OUTOFMEMORY;
 					HANDLE		hHeap = GetProcessHeap();
 					void *pvBits = HeapAlloc( hHeap, 0, bmi.bmiHeader.biWidth * 4 * bmi.bmiHeader.biHeight );
@@ -155,7 +211,7 @@ namespace cube {
 						if( GetDIBits( hdc, hbmp, 0, bmi.bmiHeader.biHeight, pvBits, &bmi, DIB_RGB_COLORS) == bmi.bmiHeader.biHeight ) {
 							ULONG	cxDelta = cxRow - bmi.bmiHeader.biWidth;
 							ARGB	*pargbMask = static_cast<ARGB *>(pvBits);
-
+							
 							for( ULONG y = bmi.bmiHeader.biHeight ; y ; --y ) {
 								for( ULONG x = bmi.bmiHeader.biWidth ; x ; --x ) {
 									if( *pargbMask++ )
@@ -169,32 +225,46 @@ namespace cube {
 						}
 						HeapFree( hHeap, 0, pvBits );
 					}
-
+					
 					return hr;
 				}
-
 			};
 		}
-
+		
+		/* ----------------------------------------------------------------- */
+		//  CShlCtxMenuVista
+		/* ----------------------------------------------------------------- */
 		class CShlCtxMenuVista : public CShlCtxMenuBase {
 		public:
+			/* ------------------------------------------------------------- */
+			//  constructor
+			/* ------------------------------------------------------------- */
 			CShlCtxMenuVista() {
 				InitIcon( MenuItem );
 			}
-
-			~CShlCtxMenuVista() {
+			
+			/* ------------------------------------------------------------- */
+			//  destructor
+			/* ------------------------------------------------------------- */
+			virtual ~CShlCtxMenuVista() {
 			}
-
+			
 		protected:
+			/* ------------------------------------------------------------- */
+			//  SetMenuIcon
+			/* ------------------------------------------------------------- */
 			void SetMenuIcon( WORD iconID, MENUITEMINFO &miinfo )
 			{
 				miinfo.fMask		|= MIIM_BITMAP;
 				miinfo.hbmpItem		= bitmaps[iconID]->GetBitmap();
 			}
-
+			
 		private:
 			std::map<WORD,clx::shared_ptr<icon::Icon> >		bitmaps;
-
+			
+			/* ------------------------------------------------------------- */
+			//  InitIcon
+			/* ------------------------------------------------------------- */
 			void InitIcon( const SUB_MENU_ITEM *smi ) {
 				for( unsigned int i = 0 ; smi[i].stringA ; ++i ) {
 					if( smi[i].iconID != ICON_NOT_USED )
