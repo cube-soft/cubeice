87e1aa13ac1002ba47b2012cd31d40e2db19edd3
clown <tt.clown@gmail.com>
2011-01-13 01:17:32 +0900

    modify 7z.dll.

--- cubeice-ctx/cubeice-ctxdata.h
+++ cubeice-ctx/cubeice-ctxdata.h
@@ -98,9 +98,9 @@ const SUB_MENU_ITEM		SubMenuDecompress[] = {
 };
 
 const SUB_MENU_ITEM		MenuItem[] = {
-	{ COMPRESS_FLAG,	IDB_LOGO,		MAKE_AWSTRING( "圧縮" ),				SubMenuCompress,	MAKE_AWSTRING( "圧縮を行う" ),			NULL },
-	{ MAIL_FLAG,		IDB_LOGO,		MAKE_AWSTRING( "圧縮してメール送信" ),	SubMenuCompAndMail,	MAKE_AWSTRING( "圧縮してメール送信" ),	NULL },
-	{ DECOMPRESS_FLAG,	IDB_LOGO,		MAKE_AWSTRING( "解凍" ),				SubMenuDecompress,	MAKE_AWSTRING( "展開を行う" ),			NULL },
+	{ COMPRESS_FLAG,	IDB_COMPRESS,		MAKE_AWSTRING( "圧縮" ),				SubMenuCompress,	MAKE_AWSTRING( "圧縮を行う" ),			NULL },
+	{ MAIL_FLAG,		IDB_COMPRESS,		MAKE_AWSTRING( "圧縮してメール送信" ),	SubMenuCompAndMail,	MAKE_AWSTRING( "圧縮してメール送信" ),	NULL },
+	{ DECOMPRESS_FLAG,	IDB_DECOMPRESS,		MAKE_AWSTRING( "解凍" ),				SubMenuDecompress,	MAKE_AWSTRING( "展開を行う" ),			NULL },
 //	{ SETTING_FLAG,		IDB_LOGO,		MAKE_AWSTRING( "設定" ),				NULL,				MAKE_AWSTRING( "設定を行う" ),			NULL },
 	{ 0, ICON_NOT_USED, NULL, NULL, NULL, NULL, NULL, NULL }		// end marker
 };
